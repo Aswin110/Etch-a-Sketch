69f91c610408b2e53c807c2ebf6c45b21e7b4a00
Aswin110 <aswinashok110@gmail.com>
2023-08-17 12:30:50 +0530

    corrected the boxes using flex

--- app.js
+++ app.js
@@ -1,18 +1,64 @@
 
-let sizeSlider = document.getElementById('sizeSlide');
-let gridContainer = document.getElementById('GridContainer');
+let sizeSlide = document.getElementById('sizeSlide');
+let gridContainer = document.getElementById('gridContainer');
+const colorPicker = document.getElementById('colorPicker');
+const colorMode = document.getElementById('colorMode');
+const clearGrid = document.getElementById('clear');
+// let box = document.querySelector('box');
 
-sizeSlider.addEventListener('input',sizeValue);
-document.getElementById('sizeValue').innerText = sizeSlider.value + ' x ' + sizeSlider.value;
+sizeSlide.addEventListener('input', sizeValue);
+colorMode.addEventListener('click', colorChange);
+clearGrid.addEventListener('click', clear);
 
-function sizeValue(){
-    document.getElementById('sizeValue').innerText = sizeSlider.value + ' x ' + sizeSlider.value; 
+document.getElementById('sizeValue').innerText = sizeSlide.value + ' x ' + sizeSlide.value;
+resetGrid();
+
+function resetGrid(){  
+    while(gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    let boxes = new Array(sizeSlide.value**2)
+    for(let i = 0; i < boxes.length; i++){
+        const box = document.createElement('div')
+        gridContainer.appendChild(box)
+        box.classList.add('box')
+        box.style = `width: ${500/sizeSlide.value}px; height: ${500/sizeSlide.value}px;` 
+        console.log('slide:',sizeSlide.value, box)
+        // box.addEventListener("mouseover", )
+    }
+    // gridContainer.style = `grid-template-rows: repeat(auto-fill, ${64/sizeSlide.value}%)`
+    // gridContainer.style = `grid-template-columns: repeat(auto-fill, ${64/sizeSlide.value}%)`
+    // console.log(64/sizeSlide.value)
 }
 
-function createBox(){
-    for(let i=0;i<=sizeSlider.value;i++){
-        const box = document.createElement('div')
-        box.classList.add('box')
-        gridContainer.append('box')
-    }
+function sizeValue(){
+    document.getElementById('sizeValue').innerText = sizeSlide.value + ' x ' + sizeSlide.value;
+    resetGrid(); 
+}
+
+function colorChange(){
+  const colorValue = colorPicker.value;
+  console.log('Selected color value:', colorValue);
+  const filledElement = document.querySelector('.filled');
+  if(filledElement){
+    document.querySelector('.filled').style.backgroundColor = colorValue;
+  }else{
+    console.log('element with class .filled not found');
+  }
+};
+
+function fillGrid(box){
+    box.classList.remove('unfilled');
+    box.classList.add('filled');   
+}
+
+function clear(){
+    boxes.forEach(box => {
+        box.classList.remove('filled');
+        box.classList.add('unfilled');  
+    });
+}
+
+function eraseGrid(){
+    
 }
